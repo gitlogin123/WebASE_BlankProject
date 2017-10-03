72f8efe5c7aef44a2424d46bf5d8826bde79b2d8
Admin <admin.selenium@repunit.com>
2017-10-03 15:38:28 +0530

    Add new While loop

--- Web-BlankProject/src/test/java/smaple/test/GitTest.java
+++ Web-BlankProject/src/test/java/smaple/test/GitTest.java
@@ -2,9 +2,11 @@ package smaple.test;
 
 public class GitTest {
 
-	public void method1(){
+	public void method1(int num){
 		System.out.println("Print..");
-		
+		while(num!=0){
+			
+		}
 		
 	}
 }
