5df7e6c05eddfe7c3f084a4e97fd376829387021
Admin <admin.selenium@repunit.com>
2017-10-04 13:53:31 +0530

    New login test Methods create

--- Web-BlankProject/src/test/java/smaple/test/PullRequest.java
+++ Web-BlankProject/src/test/java/smaple/test/PullRequest.java
@@ -1,11 +1,20 @@
 package smaple.test;
 
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
 import org.testng.annotations.Test;
 
-public class PullRequest {
+import com.framework.pagefactory.FileBasedElementLocatorFactory;
+import com.framework.pages.LoginPage;
+import com.framework.utility.WebDriverTest;
 
-	@Test
+public class PullRequest extends WebDriverTest{
+
+	@Test(enabled = false, groups = "Smoke")
 	public void openUrl(){
 		
+		ElementLocatorFactory factory = new FileBasedElementLocatorFactory(
+				bDriver.driver);
+		PageFactory.initElements(factory, new LoginPage());
 	}
 }
